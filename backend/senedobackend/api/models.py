cfd464c80cc89c18ec088c8f400c1468715e21d8
Churchjg <churchjg283@gmail.com>
2020-12-08 14:40:34 -0500

    wine model done

--- backend/senedobackend/api/models.py
+++ backend/senedobackend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Wine(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.name) + ": $" + str(self.price)
