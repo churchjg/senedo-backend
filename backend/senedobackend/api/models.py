359c6e377611ed95dd7a0137ed2f5ba4d05f87a1
Churchjg <churchjg283@gmail.com>
2020-12-08 18:50:54 -0500

    changing model

--- backend/senedobackend/api/models.py
+++ backend/senedobackend/api/models.py
@@ -7,7 +7,7 @@ class Wine(models.Model):
     year = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.PositiveIntegerField()
 
