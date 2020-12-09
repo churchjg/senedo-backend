6370c5c31f8ecd9edd3b5aba68c6fd58e5e47240
Churchjg <churchjg283@gmail.com>
2020-12-09 17:09:51 -0500

    deploying 5

--- backend/senedobackend/api/urls.py
+++ backend/senedobackend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wine', views.wine_list, name='wine_list'),
+    path('', views.wine_list, name='wine_list'),
     path('gift', views.gift_list, name='gift_list'),
 ]
