2adb5e0a756ce37532d28072a3d2c973c8c88172
Churchjg <churchjg283@gmail.com>
2020-12-09 16:32:14 -0500

    deploying to heroku

--- backend/senedobackend/api/urls.py
+++ backend/senedobackend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wine_list, name='wine_list'),
+    path('wine', views.wine_list, name='wine_list'),
     path('gift', views.gift_list, name='gift_list'),
 ]
