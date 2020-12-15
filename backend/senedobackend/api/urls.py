0c5c41a9a60b490b3d42f1dd677706bb049e94e7
Churchjg <churchjg283@gmail.com>
2020-12-15 16:12:02 -0500

    added checkout

--- backend/senedobackend/api/urls.py
+++ backend/senedobackend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wine_list, name='wine_list'),
+    path('wine', views.wine_list, name='wine_list'),
     path('gift', views.gift_list, name='gift_list'),
     path('gallery', views.gallery_list, name='gallery_list'),
     path('event', views.event_list, name='event_list'),
