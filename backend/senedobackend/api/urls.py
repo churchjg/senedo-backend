483d23563080bce551e5f4ac601eacb0a1a7351e
Churchjg <churchjg283@gmail.com>
2020-12-15 16:31:43 -0500

    changed routes

--- backend/senedobackend/api/urls.py
+++ backend/senedobackend/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    path('', views.wine_list, name='wine_list'),
     path('wine', views.wine_list, name='wine_list'),
     path('gift', views.gift_list, name='gift_list'),
     path('gallery', views.gallery_list, name='gallery_list'),
